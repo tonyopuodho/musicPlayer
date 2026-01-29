9ba653f03c83427fb653547b410a142f5c2d1520
TONY OCHIENG <tonyopuodho@gmail.com>
2026-01-29 15:33:43 +0300

    refactoring html

---
+++ musicList.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name:"Long day bud a bawl",
+        artist:"Culture",
+        image:"music.jpg",
+        src:"music/Culture - Long day bud a bawl.mp3"
+    },
+     {
+        name:"Calling Rastafari",
+        artist:"Burning Spear",
+        image:"music2.jpg",
+        src:"music/Burning Spear - Calling Rastafari.mp3"
+    },
+     {
+        name:"I Am What I Am",
+        artist:"Italis",
+        image:"reggea1.jpg",
+        src:"music/I Am What I Am.mp3"
+    },
+     {
+        name:"Peace Love and Harmony",
+        artist:"Culture",
+        image:"reggea2.jpg",
+        src:"music/Culture - Peace Love And Harmony (lyrics CC).mp3"
+    },
+     {
+        name:"Never get weary",
+        artist:"Culture",
+        image:"reggea3.jpg",
+        src:"music/Never Get Weary.mp3"
+    }
+]
